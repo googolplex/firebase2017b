5d978bd33b1de5e4830f4a22ca8c1ea703475791
googol plex <googolplex@lcompras.biz>
2017-10-07 14:52:17 -0300

    inicio de proyecto

--- codelab_android_20170831a/start_android_studio.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# hecho el 05.11.2016
-/home/xoldfusion/Downloads/android_studio/android-studio/bin/studio.sh
--- codelab_android_20170831a/ver_llaveandroidstudio.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-keytool -exportcert -alias androiddebugkey -keystore ~/.android/debug.keystore -list -v -storepass android
